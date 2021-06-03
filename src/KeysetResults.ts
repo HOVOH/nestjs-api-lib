aec13e3e567e74f6c06363403d0f172e709544a8
Michael Duchesne <20918417+Mick00@users.noreply.github.com>
2021-06-03 11:46:59 -0400

    fix out of bound error on empty keyset

--- src/KeysetResults.ts
+++ src/KeysetResults.ts
@@ -1,17 +1,22 @@
 export class KeysetResults {
   page: {
-    lastToken: string;
-    firstToken: string;
+    lastToken?: string;
+    firstToken?: string;
     size: number;
   };
   results: any[];
 
   constructor(results: any[]) {
-    this.page = {
-      lastToken: results[results.length - 1].id.toString(),
-      firstToken: results[0].id.toString(),
-      size: results.length,
-    };
-    this.results = results;
+    if (results && results.length > 0) {
+      this.page = {
+        lastToken: results[results.length - 1].id.toString(),
+        firstToken: results[0].id.toString(),
+        size: results.length,
+      };
+      this.results = results;
+    } else {
+      this.page = { size: 0 };
+      this.results = [];
+    }
   }
 }
